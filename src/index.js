940503749277e1f556b8a7748223e89d8c8b2e42
jgumerove <josh.gumerove@gmail.com>
2021-09-08 23:20:49 -0400

    render additional dom content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const api = new ApiService("http://localhost:3000")
 
 api.getWines()
-.then(wines => console.log(wines))
+.then(wines => {
+    wines.forEach(wine => {
+        const wineMarkup = `
+        <div data-id=${wine.id}>
+            <h3>${wine.name}</h3>
+            <button class="but-nope">X</button>
+            <button class="but-yay">✔</button>
+        </div>
+        <br><br>
+        `
+        document.querySelector("#card-container-list").innerHTML += wineMarkup
+    })
+})
 
