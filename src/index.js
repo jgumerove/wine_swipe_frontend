4e253fea1c42cf423773a2857b79a86f73aed5b5
jgumerove <josh.gumerove@gmail.com>
2021-09-10 11:50:10 -0400

    refactor renderWines HTML structure

--- src/index.js
+++ src/index.js
@@ -8,19 +8,17 @@ api.getWines()
 .then(wines => {
     wines.forEach(wine => {
         const wineMarkup = `
-        <li class= "card">
-        <div data-id=${wine.id}>
+        <div class="card-unswiped" data-id=${wine.id}>
             <h3>${wine.name}</h3>
             <h5>${wine.category}</h5>
             <img src=${wine.image} width="200" height="250">
             <br>
-            <button class="but-nope">X</button>
-            <button class="but-yay">✔</button>
+            <button class="yes-like">X</button>
+            <button class="no-dislike">✔</button>
         </div>
-        </li>
         <br><br>
         `
-        document.querySelector("#card-container-list").innerHTML += wineMarkup
+        document.querySelector("#card-container-list-unswiped").innerHTML += wineMarkup
     })
 })
 }
