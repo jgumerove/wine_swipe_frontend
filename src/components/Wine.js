8d2e5a54379638698995d441150c1fe3f7e9c25c
jgumerove <josh.gumerove@gmail.com>
2021-09-10 16:29:40 -0400

    refactor createWines method

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -1,12 +1,33 @@
 class Wine {
 
     constructor (wineObj) {
-        this.wineObj = wineObj
+        this.id = wineObj.id
+        this.name = wineObj.name
+        this.category = wineObj.category
+        this.image = wineObj.image
     }
 
     static createWines = () => {
-        console.log("hi")
+        api.getWines()
+        .then(wines => {
+            wines.forEach(wine => {
+                const wineMarkup = `
+        <div class="card-unswiped" data-id=${wine.id}>
+            <h3>${wine.name}</h3>
+            <h5>${wine.category}</h5>
+            <img src=${wine.image} width="200" height="250">
+            <br>
+            <button class="yes-like">X</button>
+            <button class="no-dislike">✔</button>
+        </div>
+        <br><br>
+        `
+        document.querySelector("#card-container-list-unswiped").innerHTML += wineMarkup
+            })
+        })
+
     }
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 const api = new ApiService("http://localhost:3000")
 
 
-
-
-function renderWines() {
-api.getWines()
-.then(wines => {
-    wines.forEach(wine => { //passing in each wine object
-        const wineMarkup = `
-        <div class="card-unswiped" data-id=${wine.id}>
-            <h3>${wine.name}</h3>
-            <h5>${wine.category}</h5>
-            <img src=${wine.image} width="200" height="250">
-            <br>
-            <button class="yes-like">X</button>
-            <button class="no-dislike">✔</button>
-        </div>
-        <br><br>
-        `
-        document.querySelector("#card-container-list-unswiped").innerHTML += wineMarkup
-    })
-})
-}
-
-renderWines()
 Wine.createWines()
 
