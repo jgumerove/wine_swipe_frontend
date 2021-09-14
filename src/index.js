76c002b4175304a3aac4fa2b17ee6b5971c6ce90
jgumerove <josh.gumerove@gmail.com>
2021-09-14 11:51:55 -0400

    add likeWine eventListener

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
 const api = new ApiService("http://localhost:3000")
 
-
-// Wine.createWines()
-
-
-// let addEventListeners = () => {
-//     //const unSwipedCards = document.getElementById("card-container-list-unswiped")
-//     const wineCard = document.getElementById("card-container-list-unswiped")
-//     wineCard.addEventListener("click", likeWine)
-// }
-
-// let likeWine = (e) => {
-//     console.log(e.target)
-//     //e.target.className = "card-swiped"
-// }
-
-// addEventListeners()
-
+    
 //create all the wines below
-function createWines () {
-    api.getWines()
-    .then(wines => {
-        wines.forEach(wine => {
-            renderWine(wine)
-        })
+    function createWines () {
+        api.getWines()
+        .then(wines => {
+            wines.forEach(wine => {
+                renderWine(wine)
+            })
     })
-
+    
 }
 
 //render a specific wine 
 function renderWine(wine) {
     const wineMarkup = `
     <div class="card-unswiped" data-id=${wine.id}>
-        <h3>${wine.name}</h3>
-        <h5>${wine.category}</h5>
+    <h3>${wine.name}</h3>
+    <h5>${wine.category}</h5>
         <img src=${wine.image} width="200" height="250">
         <br>
         <button class="yes-like">X</button>
         <button class="no-dislike">✔</button>
-    </div>
-    <br><br>
-    `
-    document.querySelector("#card-container-list-unswiped").innerHTML += wineMarkup
-}
-//call the function above 
-createWines()
+        </div>
+        <br><br>
+        `
+        document.querySelector("#card-container-list-unswiped").innerHTML += wineMarkup
+    }
+    
+    function addEventListeners () {
+        const wineCard = document.getElementById("card-container-list-unswiped")
+        wineCard.addEventListener("click", likeWine)
+    }
 
-
+    function likeWine(e) {
+        if (e.target.tagName == "BUTTON") {
+            const changeClass = e.target.closest(".card-unswiped").remove(".card-unswiped").add(".card-swiped")
+        }
+    }
+    
+    //call the function above 
+    //note the renderWine function is in the createWines function 
+    createWines()
+    addEventListeners()
+    
+    
+    
