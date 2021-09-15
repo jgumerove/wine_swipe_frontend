576c589a31e5c24fde7f98f9a8a7c12e57c58f7a
jgumerove <josh.gumerove@gmail.com>
2021-09-15 13:08:20 -0400

    refactor OpenWineModelForm to prevent duplication

--- src/components/Wine.js
+++ src/components/Wine.js
@@ -61,6 +61,7 @@ class Wine {
 
    //opens form to add a new wine
     static openWineModelForm = () => {
+        modal.mainModal.innerHTML = ""
         modal.mainModal.innerHTML += `
       <form>
         <label for="fname">First name:</label><br>
