d2f53d72f28247b2a10b50e365d57ac95562a6b7
jgumerove <josh.gumerove@gmail.com>
2021-09-16 12:50:41 -0400

    create method to get main likes modal content

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,34 +7,38 @@ class Modal {
     //grab the element
     get modalElement () {
         return document.querySelector("#myModal")
-    }
+    };
 
     get modalElementLikes () {
         return document.querySelector("#myModalLikes")
-    }
+    };
     
     //grab the main part to add content to
     get mainModal () {
         return document.getElementById("modal-main")
+    };
+
+    get mainModalLikes () {
+        return document.getElementById("modal-main-likes")
     }
 
     //use the getter to open element by displaying in block
     open = () => {
         this.modalElement.style.display = "block"
-    }
+    };
 
     openLikes = () => {
         this.modalElementLikes.style.display = "block"
-    }
+    };
 
     //use getter to close by changing display to none 
     close = () => {
         this.modalElement.style.display = "none"
-    }
+    };
 
     closeLikes = () => {
         this.modalElementLikes.style.display = "none"
-    }
+    };
 
     addCloseEventListener = () => {
         this.modalElement.addEventListener("click", (e) => {
@@ -42,6 +46,6 @@ class Modal {
                 this.close()
             }
         })
-    }
+    };
     
 }
