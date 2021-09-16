570b28554a183a812caa66df0a4cb9e5f90dd269
jgumerove <josh.gumerove@gmail.com>
2021-09-16 11:48:18 -0400

    create methods to get and open likes modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,6 +8,10 @@ class Modal {
     get modalElement () {
         return document.querySelector("#myModal")
     }
+
+    get modalElementLikes () {
+        return document.querySelector("#myModalLikes")
+    }
     
     //grab the main part to add content to
     get mainModal () {
@@ -17,6 +21,10 @@ class Modal {
     //use the getter to open element by displaying in block
     open = () => {
         this.modalElement.style.display = "block"
+    }
+
+    openLikes = () => {
+        this.modalElementLikes.style.display = "block"
     }
 
     //use getter to close by changing display to none 
